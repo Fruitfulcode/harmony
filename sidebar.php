8a2e94020f94ab59d07a7a02e91e8f48fcfb8585
IvanRyzhik <ryzhik@fruitfulcode.com>
2016-01-29 16:01:18 +0200

    Style fixing

--- sidebar.php
+++ sidebar.php
@@ -11,9 +11,9 @@
 			<?php do_action( 'before_sidebar' ); ?>
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-				<!-- <aside id="search" class="widget widget_search">
-					<?php get_search_form(); ?>
-				</aside>
+			<!-- 	<aside id="search" class="widget widget_search">
+				<?php get_search_form(); ?>
+			</aside> -->
 
 				<aside id="archives" class="widget">
 					<h1 class="widget-title"><?php _e( 'Archives', 'fruitful' ); ?></h1>
@@ -29,7 +29,7 @@
 						<li><?php wp_loginout(); ?></li>
 						<?php wp_meta(); ?>
 					</ul>
-				</aside> -->
+				</aside>
 
 			<?php endif; // end sidebar widget area ?>
 		</div><!-- #secondary .widget-area -->
